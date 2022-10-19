44fa6fc2cffb7ec4ec9c73f625a41c6b345f1863
dhrxogus03 <110527190+dhrxogus03@users.noreply.github.com>
2022-10-19 19:54:56 +0900

    Update Team ASC 프로젝트 관리 테이블.sql

--- 옥태현/Team ASC 프로젝트 관리 테이블.sql
+++ 옥태현/Team ASC 프로젝트 관리 테이블.sql
@@ -1,5 +1,5 @@
 create table user(
-seq integer auto_increment primary key,
+user_seq integer auto_increment primary key,
 id varchar(50) not null default(''),
 pwd varchar(50) not null default(''),
 name varchar(50) not null default(''),
@@ -13,7 +13,7 @@ update_time datetime
 ); 
 
 create table team_member(
-seq integer auto_increment primary key,
+team_member_seq integer auto_increment primary key,
 user_seq integer not null default(0),
 team_id varchar(50) not null default(''),
 team_name varchar(50) not null default(''),
@@ -26,7 +26,7 @@ update_time datetime
 );
 
 create table message(
-seq integer auto_increment primary key,
+message_seq integer auto_increment primary key,
 from_user_seq integer not null default(''),
 to_user_seq integer not null default(''),
 content text not null default(''),
@@ -39,7 +39,7 @@ update_time datetime
 );
 
 create table document(
-seq integer auto_increment primary key,
+document_seq integer auto_increment primary key,
 project_seq integer not null default(0),
 document_name varchar(50) not null default(''),
 document_category varchar(10) not null default(''),
@@ -51,7 +51,7 @@ update_time datetime
 );
 
 create table project(
-seq integer auto_increment primary key,
+project_seq integer auto_increment primary key,
 team_id varchar(50) not null default(''),
 project_name varchar(100) not null default(''),
 finish_yn varchar(1) not null default('N'),
@@ -63,12 +63,12 @@ update_time datetime
 );
 
 create table calendar(
-seq integer auto_increment primary key,
+calendar_seq integer auto_increment primary key,
 project_seq integer not null default(0),
 calendar_category varchar(20) not null default(''),
 start_date varchar(8) not null default(''),
 end_date varchar(8) not null default(''),
-content varchar(100) not null default(''),
+content varchar(50) not null default(''),
 finish_yn varchar(1) not null default('N'),
 
 insert_time datetime default(now()),
